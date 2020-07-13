5d22b6d9087122b6468b55395bcf503d0d5612f8
Danny Mota <dannymota@fb.com>
2020-07-13 18:09:29 -0400

    Create Bottom Nav View

--- app/src/main/java/com/example/viyer/MainActivity.java
+++ app/src/main/java/com/example/viyer/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.viyer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+                bottomNavigationView = findViewById(R.id.bottomNavigation);
+        fragmentManager = getSupportFragmentManager();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_browse:
+                        Log.d(TAG, "Clicked browse");
+                        break;
+                    case R.id.action_chat:
+                        Log.d(TAG, "Clicked chat");
+                        break;
+                    case R.id.action_post:
+                        Log.d(TAG, "Clicked post");
+                        break;
+                    case R.id.action_selling:
+                        Log.d(TAG, "Clicked selling");
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Log.d(TAG, "Clicked profile");
+                        break;
+                }
+                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_browse);
     }
 }
