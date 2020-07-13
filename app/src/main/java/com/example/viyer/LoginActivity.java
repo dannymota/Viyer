3b678b00c23e71982e585b06cf3010815be56ca9
Danny Mota <dannymota@fb.com>
2020-07-13 19:34:02 -0400

    Add TAG to LoginActivity

--- app/src/main/java/com/example/viyer/LoginActivity.java
+++ app/src/main/java/com/example/viyer/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
