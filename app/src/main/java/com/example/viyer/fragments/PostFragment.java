ece44cc6711f2ff465606a66ec80b0530b7f1eba
Danny Mota <dannymota@fb.com>
2020-07-20 11:17:56 -0400

    Fix take photo returning last photo bug

--- app/src/main/java/com/example/viyer/fragments/PostFragment.java
+++ app/src/main/java/com/example/viyer/fragments/PostFragment.java
@@ -1,22 +1,12 @@
 package com.example.viyer.fragments;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-import androidx.fragment.app.Fragment;
-
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -27,6 +17,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import com.example.viyer.R;
 import com.example.viyer.layouts.ImagePreviewLayout;
@@ -39,10 +36,14 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 import static android.app.Activity.RESULT_OK;
@@ -66,8 +67,7 @@ public class PostFragment extends Fragment {
 
     private LinearLayout llPreviews;
     private List<Uri> filePaths;
-    private File photoFile;
-    private String photoFileName = "photo.jpg";
+    private Uri takePhoto;
     private final int PICK_IMAGE_REQUEST = 22;
     public final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 40;
     private Button btnSelect, btnTake, btnPost;
@@ -75,6 +75,8 @@ public class PostFragment extends Fragment {
     private StorageReference storageReference;
     private ImageView ivPreview;
     private FirebaseUser user;
+
+    private String imageFilePath;
 
     public PostFragment() {
         // Required empty public constructor
@@ -134,6 +136,13 @@ public class PostFragment extends Fragment {
         actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
         actionBar.setTitle("Post");
 
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+
         btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -147,13 +156,6 @@ public class PostFragment extends Fragment {
                 uploadImage();
             }
         });
-
-        btnTake.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchCamera();
-            }
-        });
     }
 
     public ImagePreviewLayout createImagePreview(Bitmap imgPhoto, Boolean isMainPhoto) {
@@ -164,41 +166,40 @@ public class PostFragment extends Fragment {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Image from here..."), PICK_IMAGE_REQUEST);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
+
+//    public void launchCamera() {
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Create a File reference for future access
+//        photoFile = getFileUri(photoFileName);
+//
+//        // wrap File object into a content provider
+//        // required for API >= 24
+//        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+//        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.viyer.contentprovider", photoFile);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+//
+//        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+//        // So as long as the result is not null, it's safe to use the intent.
+//        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+//            // Start the image capture intent to take photo
+//            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+//        }
+//    }
 
     public void launchCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.viyer.contentprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            // Start the image capture intent to take photo
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
     }
 
-    public File getFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
     @Override
@@ -207,22 +208,20 @@ public class PostFragment extends Fragment {
         Bitmap bitmap;
         Boolean isMainPhoto;
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            // Get the Uri of data
             try {
-                // Setting image on image view using Bitmap
                 isMainPhoto = filePaths.size() == 0 ? true : false;
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
                 filePaths.add(data.getData());
-                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePaths.get(filePaths.size() - 1));
                 ivPreview.setImageBitmap(bitmap);
                 llPreviews.addView(createImagePreview(bitmap, isMainPhoto));
             } catch (IOException e) {
-                // Log the exception
                 e.printStackTrace();
             }
-        } else if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+        } else if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data.getData() == null) {
             isMainPhoto = filePaths.size() == 0 ? true : false;
-            filePaths.add(Uri.fromFile(photoFile));
-            bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            filePaths.add(getImageUri(getContext(), bitmap));
             ivPreview.setImageBitmap(bitmap);
             llPreviews.addView(createImagePreview(bitmap, isMainPhoto));
         }
@@ -230,20 +229,12 @@ public class PostFragment extends Fragment {
 
     private void uploadImage() {
         if (filePaths != null) {
-            // Code for showing progressDialog while uploading
-            final ProgressDialog progressDialog
-                    = new ProgressDialog(getContext());
+            final ProgressDialog progressDialog = new ProgressDialog(getContext());
             progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            // Defining the child of storageReference
-            StorageReference ref;
-
-            // adding listeners on upload
-            // or failure of image
-
             for (Uri uri : filePaths) {
-                ref = storageReference.child("posts/" + user.getUid() + "/" + UUID.randomUUID().toString());
+                StorageReference ref = storageReference.child("posts/" + user.getUid() + "/" + UUID.randomUUID().toString());
                 ref.putFile(uri)
                         .addOnSuccessListener(
                                 new OnSuccessListener<UploadTask.TaskSnapshot>() {
